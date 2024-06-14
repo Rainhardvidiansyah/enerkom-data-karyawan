8b7a78f8f7e53987339b8b78b45a9cf10a458fd5
Rainhardvidiansyah <rainhardvidians@gmail.com>
2024-06-15 02:06:54 +0700

    Cari role user dengan metode role

--- src/main/java/com/enerkom/karyawan/service/RegistrationService.java
+++ src/main/java/com/enerkom/karyawan/service/RegistrationService.java
@@ -1,7 +1,10 @@
 package com.enerkom.karyawan.service;
 
 
+import com.enerkom.karyawan.entity.Roles;
 import com.enerkom.karyawan.entity.Users;
+import com.enerkom.karyawan.enums.ERole;
+import com.enerkom.karyawan.repository.RoleRepository;
 import com.enerkom.karyawan.repository.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,10 +12,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class RegistrationService {
 
     private static final Logger log = LoggerFactory.getLogger(RegistrationService.class);
+
+    @Autowired
+    private RoleRepository roleRepository;
+
 
     @Autowired
     private UserRepository userRepository;
@@ -21,12 +33,22 @@ public class RegistrationService {
     private PasswordEncoder passwordEncoder;
 
 
+    public List<Roles> role(){
+
+        List<Roles> roles = new ArrayList<>();
+
+        Optional<Roles> optional = this.roleRepository.findRolesByERole(ERole.ROLE_EMPLOYEE.name());
+
+        roles.add(optional.get());
+        return roles;
+    }
 
 
     public Users registration(String email, String password){
         Users users = new Users();
         users.setEmail(email);
         users.setPassword(passwordEncoder.encode(password));
+        users.setRoles(this.role());
         return userRepository.save(users);
     }
 
